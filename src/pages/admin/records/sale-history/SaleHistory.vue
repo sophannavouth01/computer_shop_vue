367c7258eeb080346ef682611ffe837cda6235a3
Sophanna <sophanna@Sophannas-MacBook-Pro.local>
2024-02-06 22:37:14 +0700

    UPDATE

--- src/pages/admin/records/sale-history/SaleHistory.vue
+++ src/pages/admin/records/sale-history/SaleHistory.vue
@@ -35,10 +35,10 @@
           </tr>
       </thead>
       <tbody>
-        <tr v-for="product in ProductData" :key="product.id"
+        <tr v-for="(product,index) in ProductData" :key="product.id"
         class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 hover:bg-blue-400">
             <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-              {{product.order_id }}
+              {{index+1}}
 
             </th>
             <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
